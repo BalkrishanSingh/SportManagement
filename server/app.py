5fc63712df00d38a1ccea1c6c0e696433703af95
Balkrishan Singh <Balkrishan62005@gmail.com>
2023-09-23 12:50:35 +0530

    added material-tailwind/react package

--- server/app.py
+++ server/app.py
@@ -20,6 +20,18 @@ class EquipmentSchema(ma.Schema):
 equipment_schema = EquipmentSchema()
 equipments_schema =  EquipmentSchema(many=True)
 
+class Equipments(db.Model):
+    sno = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(128),nullable = False)
+    amount = db.Column(db.Integer)
+
+class EquipmentSchema(ma.Schema):
+    class Meta:
+        fields  = ('sno','name','amount')
+        
+equipment_schema = EquipmentSchema()
+equipments_schema =  EquipmentSchema(many=True)
+
 @app.route("/equipment",methods=["GET","POST"])
 def equipment_api():
     if request.method == "GET":
@@ -51,7 +63,39 @@ def equipment_api(id):
         db.session.delete(equipment)
         db.session.commit()
         return equipment_schema.jsonify(equipment)
+
+@app.route("/equipment",methods=["GET","POST"])
+def equipment_api():
+    if request.method == "GET":
+        equipments = Equipments.query.all()
+        data = equipments_schema.dump(equipments)
+        return jsonify(data)
+    elif request.method == "POST":
+        equipment = Equipments(request.json["name"],request.json["amount"])
+        db.session.add(equipment)
+        db.session.commit()
+        return equipment_schema.jsonify(equipment)
     
+@app.route("/equipment/<id>",methods=["GET","PUT","DELETE"])
+def equipment_api(id):
+    if request.method == "GET":
+        equipment = Equipments.query.get(id)
+        return equipment_schema.jsonify(equipment)
+    
+    elif request.method == "PUT":
+        equipment = Equipments.query.get(id)
+        equipment.name = request.json["name"]
+        equipment.amount = request.json["amount"]
+        db.session.add(equipment)
+        db.session.commit()
+        return equipment_schema.jsonify(equipment)
+    
+    elif request.method == "DELETE":
+        equipment = Equipments.query.get(id)
+        db.session.delete(equipment)
+        db.session.commit()
+        return equipment_schema.jsonify(equipment)
+
 
 if __name__ == "__main__":
     app.run(debug=True,port=8000)
